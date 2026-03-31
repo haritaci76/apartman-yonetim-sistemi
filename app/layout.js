3528d528ab811912170f6bd153ccf3366cf62def
haritaci76 <146091087+haritaci76@users.noreply.github.com>
2026-03-31 14:22:31 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Apartman Yönetim Sistemi",
+  description: "Profesyonel Site ve Apartman Yönetimi",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr">
+      <body>{children}</body>
+    </html>
+  );
+}
